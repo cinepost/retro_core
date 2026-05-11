cb179a435fd0c149e433e6eb90dc47172cef04e7
Max Kirienko <kirienko.max@gmail.com>
2026-05-11 23:56:04 +0300

    Initial commit with .gitignore

---
+++ src/core.cpp
@@ -0,0 +1,298 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+
+#include "libretro.h"
+#include "renderer_vdp8.h"
+
+#define FRAMEBUFFER_WIDTH 448
+#define FRAMEBUFFER_HEIGHT 252
+
+static RetroCore::VDP8 gRenderer;
+static struct retro_log_callback logging;
+static retro_log_printf_t log_cb;
+
+static void fallback_log(enum retro_log_level level, const char *fmt, ...) {
+   (void)level;
+   va_list va;
+   va_start(va, fmt);
+   vfprintf(stderr, fmt, va);
+   va_end(va);
+}
+
+void retro_init(void) {
+   const bool result = gRenderer.init(FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT);
+   assert(result);
+   if(!result) {
+      log_cb(RETRO_LOG_ERROR, "RetroCore::Renderer initializatoin failed !\n");
+   }
+}
+
+void retro_deinit(void) {
+   gRenderer.deinit();
+}
+
+unsigned retro_api_version(void) {
+   return RETRO_API_VERSION;
+}
+
+void retro_set_controller_port_device(unsigned port, unsigned device) {
+   log_cb(RETRO_LOG_INFO, "Plugging device %u into port %u.\n", device, port);
+}
+
+void retro_get_system_info(struct retro_system_info *info) {
+   memset(info, 0, sizeof(*info));
+   info->library_name     = "TestCore";
+   info->library_version  = "v1";
+   info->need_fullpath    = false;
+   info->valid_extensions = NULL; // Anything is fine, we don't care.
+}
+
+static retro_video_refresh_t video_cb;
+static retro_audio_sample_t audio_cb;
+static retro_audio_sample_batch_t audio_batch_cb;
+static retro_environment_t environ_cb;
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
+
+void retro_get_system_av_info(struct retro_system_av_info *info) {
+   float aspect = static_cast<float>(FRAMEBUFFER_WIDTH) / static_cast<float>(FRAMEBUFFER_HEIGHT);
+
+   info->timing = (struct retro_system_timing) {
+      .fps = 60.0,
+      .sample_rate = 0.0,
+   };
+
+   info->geometry = (struct retro_game_geometry) {
+      .base_width   = FRAMEBUFFER_WIDTH,
+      .base_height  = FRAMEBUFFER_HEIGHT,
+      .max_width    = FRAMEBUFFER_WIDTH,
+      .max_height   = FRAMEBUFFER_HEIGHT,
+      .aspect_ratio = aspect,
+   };
+
+}
+
+void retro_set_environment(retro_environment_t cb) {
+   environ_cb = cb;
+
+   bool no_content = true;
+   cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &no_content);
+
+   if (cb(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &logging)) {
+      log_cb = logging.log;
+   } else {
+      log_cb = fallback_log;
+   }
+}
+
+void retro_set_audio_sample(retro_audio_sample_t cb) {
+   audio_cb = cb;
+}
+
+void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb) {
+   audio_batch_cb = cb;
+}
+
+void retro_set_input_poll(retro_input_poll_t cb) {
+   input_poll_cb = cb;
+}
+
+void retro_set_input_state(retro_input_state_t cb) {
+   input_state_cb = cb;
+}
+
+void retro_set_video_refresh(retro_video_refresh_t cb) {
+   video_cb = cb;
+}
+
+static unsigned x_coord;
+static unsigned y_coord;
+
+void retro_reset(void) {
+   gRenderer.reset();
+}
+
+static void update_input(void) {
+   static const uint16_t sKbdMouseMovementIncrement = 1;
+   input_poll_cb();
+
+   if (input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP)) {
+      /* Stub */
+   }
+
+   int16_t mouse_delta_x = 0;
+   int16_t mouse_delta_y = 0;
+
+   // Query keyboard keys using libretro key IDs
+   bool up    = input_state_cb(0, RETRO_DEVICE_KEYBOARD, 0, RETROK_UP);
+   bool down  = input_state_cb(0, RETRO_DEVICE_KEYBOARD, 0, RETROK_DOWN);
+   bool left  = input_state_cb(0, RETRO_DEVICE_KEYBOARD, 0, RETROK_LEFT);
+   bool right = input_state_cb(0, RETRO_DEVICE_KEYBOARD, 0, RETROK_RIGHT);
+
+   if (up)    mouse_delta_y -= sKbdMouseMovementIncrement;
+   if (down)  mouse_delta_y += sKbdMouseMovementIncrement;
+   if (left)  mouse_delta_x -= sKbdMouseMovementIncrement;
+   if (right) mouse_delta_x += sKbdMouseMovementIncrement;
+
+   // Query mouse
+   mouse_delta_x = input_state_cb(0, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_X);
+   mouse_delta_y = input_state_cb(0, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_Y);
+   bool mouse_left_pressed = input_state_cb(0, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_LEFT);
+   bool mouse_right_pressed = input_state_cb(0, RETRO_DEVICE_MOUSE, 0, RETRO_DEVICE_ID_MOUSE_RIGHT);
+
+   gRenderer.moveDebugCursor(mouse_delta_x, mouse_delta_y);
+}
+
+static void render(void) {
+   /* Try rendering straight into VRAM if we can. */
+   const uint8_t *buf = nullptr;
+   uint32_t stride_bytes = gRenderer.getFramebufferStride();
+   struct retro_framebuffer fb = {0};
+   fb.width = gRenderer.getFramebufferWidth();
+   fb.height = gRenderer.getFramebufferHeight();
+   fb.access_flags = RETRO_MEMORY_ACCESS_WRITE;
+   if (environ_cb(RETRO_ENVIRONMENT_GET_CURRENT_SOFTWARE_FRAMEBUFFER, &fb) && fb.format == RETRO_PIXEL_FORMAT_XRGB8888) {
+      stride_bytes = fb.pitch;
+      gRenderer.clearFramebuffer(static_cast<uint8_t*>(fb.data), stride_bytes);
+      buf = gRenderer.render(static_cast<uint8_t*>(fb.data), stride_bytes);
+   } else {
+      gRenderer.clearFramebuffer();
+      buf = gRenderer.render();
+   }
+
+   assert(buf);
+   video_cb(buf, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, stride_bytes);
+}
+
+static void check_variables(void) {
+}
+
+static void audio_callback(void) {
+   audio_cb(0, 0);
+}
+
+void update_refresh_rate(double new_fps) {
+   static double current_fps = 0.0;
+   
+   if(current_fps == new_fps) return;
+
+   struct retro_system_av_info av_info;
+   retro_get_system_av_info(&av_info);
+   av_info.timing.fps = new_fps;
+
+   if (environ_cb(RETRO_ENVIRONMENT_SET_SYSTEM_AV_INFO, &av_info)) {
+      current_fps = new_fps;
+      log_cb(RETRO_LOG_INFO, "Refresh rate updated to: %f\n", new_fps);
+   }
+}
+
+void retro_run(void) {
+   assert(gRenderer.isInitialized());
+   update_refresh_rate(1000.0); // debug fps
+   update_input();
+   render();
+   audio_callback();
+
+   bool updated = false;
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated) {
+      check_variables();
+   }
+}
+
+
+bool retro_load_game(const struct retro_game_info *info) {
+   enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_XRGB8888;
+   if (!environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &fmt)) {
+      log_cb(RETRO_LOG_INFO, "XRGB8888 is not supported.\n");
+      return false;
+   }
+
+   struct retro_keyboard_callback cb;
+   cb.callback = [](bool down, unsigned keycode, uint32_t character, uint16_t mod) {
+      if (down) {
+         // Example: Detect Ctrl + R
+         if ((mod & RETROKMOD_CTRL) != 0 && keycode == RETROK_r) {
+            gRenderer.setShowDebugInfoState(!gRenderer.getShowDebugInfoState());
+         }
+      }
+   };
+
+   // Register with the frontend
+   if (!environ_cb(RETRO_ENVIRONMENT_SET_KEYBOARD_CALLBACK, &cb)) {
+      log_cb(RETRO_LOG_INFO, "Error setting frontend callback!\n");
+   }
+
+   check_variables();
+
+   (void)info;
+   return true;
+}
+
+void retro_unload_game(void) {
+}
+
+unsigned retro_get_region(void) {
+   return RETRO_REGION_NTSC;
+}
+
+bool retro_load_game_special(unsigned type, const struct retro_game_info *info, size_t num) {
+   if (type != 0x200) {
+      return false;
+   }
+   
+   if (num != 2) {
+      return false;
+   }
+
+   return retro_load_game(NULL);
+}
+
+size_t retro_serialize_size(void) {
+   return 2;
+}
+
+bool retro_serialize(void *data_, size_t size) {
+   if (size < 2) {
+      return false;
+   }
+
+   uint8_t *data = static_cast<uint8_t*>(data_);
+   data[0] = x_coord;
+   data[1] = y_coord;
+   return true;
+}
+
+bool retro_unserialize(const void *data_, size_t size) {
+   if (size < 2) {
+      return false;
+   }
+
+   const uint8_t *data = static_cast<const uint8_t*>(data_);
+   x_coord = data[0] & 31;
+   y_coord = data[1] & 31;
+   return true;
+}
+
+void *retro_get_memory_data(unsigned id) {
+   (void)id;
+   return NULL;
+}
+
+size_t retro_get_memory_size(unsigned id) {
+   (void)id;
+   return 0;
+}
+
+void retro_cheat_reset(void) {
+
+}
+
+void retro_cheat_set(unsigned index, bool enabled, const char *code) {
+   (void)index;
+   (void)enabled;
+   (void)code;
+}
---
+++ src/renderer.h
@@ -0,0 +1,105 @@
+#ifndef __RETRO_CORE_RENDERER_H
+#define __RETRO_CORE_RENDERER_H
+
+#include "debug_data.h"
+
+#include <cstdint>
+#include <vector>
+#include <cassert>
+
+
+namespace RetroCore {
+
+class Renderer {
+	public:
+		struct SpriteBase {
+			uint8_t width;
+			uint8_t height;
+		};
+
+		struct Coord {
+			uint32_t x;
+			uint32_t y;
+
+			Coord(): x(0u), y(0u) {}
+			Coord(uint32_t _x, uint32_t _y): x(_x), y(_y) {}
+		};
+
+		struct CoordRel {
+			int x;
+			int y;
+
+			CoordRel(): x(0), y(0) {}
+		};
+
+		enum class BlitMode { 
+			REPLACE,
+			XOR,
+			OVER, 
+		};
+
+		using CursorType = DebugData::CursorType;
+
+		Renderer();
+
+		bool init(uint16_t framebuffer_width, uint16_t framebuffer_height);
+		void deinit();
+		void reset();
+
+		const uint8_t* render();
+		const uint8_t* render(uint8_t* pFrameData, uint32_t stride_bytes);
+
+		void  clearFramebuffer();
+		void  clearFramebuffer(uint8_t* pFrameData, uint32_t stride_bytes);
+
+		bool  isInitialized() const { return mIsInitialized; }
+
+		uint32_t getFramebufferStride() const { return mFramebufferStride; }
+		uint16_t getFramebufferWidth() const { return mFramebufferWidth; }
+		uint16_t getFramebufferHeight()const { return mFramebufferHeight; }
+
+		void setShowDebugInfoState(bool state) { mShowDebugInfo = state; }
+		bool getShowDebugInfoState() const { return mShowDebugInfo; }
+		void setDebugBackgroundPos(uint32_t x, uint32_t y);
+		void setDebugCursorPos(uint32_t x, uint32_t y);
+		void moveDebugCursor(int16_t x_delta, int16_t y_delta);
+		void setDebugCursorType(CursorType t) { mDebugCursorType = t; }
+
+		const Coord& getDebugBackgroundPos() const { return mDebugBackgroundPos; }
+		const Coord& getDebugCursorPos() const { return mDebugCursorPos; }
+
+	protected:
+		virtual bool renderImpl(uint8_t* pFrameData, uint32_t stride_bytes) = 0;
+
+	private:
+		bool _render(uint8_t* pFrameData, uint32_t stride_bytes, bool use_internal_buffer);
+		void drawDebugBackground(uint8_t* pFrameData, uint32_t stride_bytes, uint32_t square_size = 16, uint32_t color1 = 0x00007F00, uint32_t color2 = 0x007F0000);
+		void drawDebugCursor(uint8_t* pFrameData, uint32_t stride_bytes);
+
+		template <BlitMode M>
+		void blit(uint8_t* pFrameData, uint32_t stride_bytes, const uint8_t* pSrcData, uint16_t src_width, uint16_t src_height, int16_t dst_pos_x, int16_t dst_pos_y);
+		
+		void invertPixel(uint8_t* pFrameData, uint32_t stride_bytes, uint16_t x, uint16_t y);
+
+	private:
+		bool		mIsInitialized;
+		bool        mIsFramebufferClear;
+		bool        mIsExternalFramebufferClear;
+
+		uint16_t 	mFramebufferWidth;
+		uint16_t 	mFramebufferHeight;
+		uint8_t     mPixelStride;
+		uint32_t    mFramebufferStride;
+		size_t      mFramebufferDataSize;
+
+		bool        mShowDebugInfo;
+		CursorType  mDebugCursorType;
+		Coord       mDebugBackgroundPos;
+		Coord       mDebugCursorPos;
+
+		std::vector<uint8_t> mFramebuffer;
+};
+
+}  // namespace RetroCore
+
+#endif  // __RETRO_CORE_RENDERER_H
